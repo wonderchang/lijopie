f46d2c1f96672a4cec2b3c81c763e14f927bc122
Wonder Chang <iwonder.tw@gmail.com>
2015-04-27 00:03:05 +0800

    Prepare the data for requesting to the police system

--- app/php/add-report.php
+++ app/php/add-report.php
@@ -33,10 +33,21 @@ $result = mysql_query("INSERT INTO report SET
 
 if(!$result) { echo 2; return; }
 
-echo 1;
-/*
+$result = mysql_query("SELECT * FROM region WHERE region_id=$region_id");
+$row = mysql_fetch_assoc($result);
+$police_code = $row['police_code'];
+
+$result = mysql_query("SELECT * FROM subject WHERE subject_id=$subject_id");
+$row = mysql_fetch_assoc($result);
+$subject_name = $row['subject_name'];
+
+$result = mysql_query("SELECT * FROM session ORDER BY session_id DESC LIMIT 1");
+$row = mysql_fetch_assoc($result);
+$verify_code = $row['verify_code'];
+$session = $row['cookie_key'].'='.$row['cookie_value'];
+
 $data = array(
-  'nfile1'   => '@'.realpath("../$photo"),
+  'nfile1'   => '',
   'nfile2'   => '',
   'nfile3'   => '',
   'name'     => 'lijopie',
@@ -45,13 +56,17 @@ $data = array(
   'tel'      => '請輸入電話',
   'job'      => '請輸入職業',
   'address'  => '請輸入地址',
-  'subject'  => '彎道停車',
-  'region'   => 'ou=01,ou=tncgb02,o=tncgb,c=tw',
+  'subject'  => $subject_name,
+  'region'   => $police_code,
   'content1' => '',
-  'content'  => '車內無人，崇明路，文化派出所旁',
-  'chkint'   => '81233'
+  'content'  => $content,
+  'chkint'   => $verify_code
 );
+if($picture1 !== '') { $data['nfile1'] = '@'.realpath("../$picture1"); }
+if($picture2 !== '') { $data['nfile2'] = '@'.realpath("../$picture2"); }
+if($picture3 !== '') { $data['nfile3'] = '@'.realpath("../$picture3"); }
 
+/*
 $url = 'http://www.tnpd.gov.tw/chinese/home.jsp?menudata=TncgbMenu&mserno=201012130066&serno=201012130069&serno3=&serno4=&contlink=ap/mail1_save_new.jsp';
 $user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36";
 $ch = curl_init($url);
@@ -59,10 +74,14 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
 curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_COOKIE, 'JSESSIONID=aevmgy424i7b');
+curl_setopt($ch, CURLOPT_COOKIE, $session);
 $result = curl_exec($ch);
-$fp = fopen("$cookie-{$time()}.html");
-var_dump($result);
+$response_file = "../response/$cookie-".time().'.html';
+$fp = fopen($response_file, 'w');
+fwrite($fp, $result);
+fclose($fp);
 curl_close($ch);
- */
+*/
+
+echo 1;
 ?>
