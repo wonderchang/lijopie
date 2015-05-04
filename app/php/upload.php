1c2fee39db7004f50683059d1385a7cdb0396d8c
slighter12 <plokm.slighter@gmail.com>
2015-05-04 22:13:52 +0800

    add application which will change image's size when user upload image, now I default changing weight to 1080 pixel

--- app/php/upload.php
+++ app/php/upload.php
@@ -5,8 +5,11 @@ if (isset($_FILES['photo'])) {
   list($type, $ext) = explode("/", $_FILES['photo']['type']);
   $name = md5(uniqid(rand(), true).$_FILES['photo']['name']).'.'.$ext;
   move_uploaded_file($_FILES['photo']['tmp_name'], "../uploads/$name");
+
   $time = date('Y-m-d H:i:s');
+  $cmd0 = "convert ../uploads/$name -resize 1080 ../uploads/$name";
   $cmd = "convert ../uploads/$name -channel RGBA -fill orange -stroke black -gravity SouthEast -pointsize 64 -annotate +10+5 '$time' ../uploads/$name";
+  exec($cmd0);
   exec($cmd);
   echo $name;
 }
